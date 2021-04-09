de3bf31b6912bc03b2e7502570ab756ad0ac6f91
mow-mow <78189264+mow-mow@users.noreply.github.com>
2021-04-09 14:23:13 +0900

    ログ仕込み

--- src/main/java/info/caprese/capelline/CapellineApplication.java
+++ src/main/java/info/caprese/capelline/CapellineApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@Configuration
 public class CapellineApplication {
 
     public static void main(String[] args) {
