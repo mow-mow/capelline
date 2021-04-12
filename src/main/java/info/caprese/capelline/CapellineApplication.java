34a51ee77a93450c2587e77d4430ac5ea6f65f8a
mow-mow <78189264+mow-mow@users.noreply.github.com>
2021-04-12 10:02:17 +0900

    戻し

--- src/main/java/info/caprese/capelline/CapellineApplication.java
+++ src/main/java/info/caprese/capelline/CapellineApplication.java
@@ -1,19 +1,9 @@
 package info.caprese.capelline;
 
 
-import info.caprese.capelline.config.CustomRestTemplateCustomizer;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -23,8 +13,4 @@ public class CapellineApplication {
         SpringApplication.run(CapellineApplication.class, args);
     }
 
-    @Bean
-    public CustomRestTemplateCustomizer customRestTemplateCustomizer() {
-        return new CustomRestTemplateCustomizer();
-    }
 }
